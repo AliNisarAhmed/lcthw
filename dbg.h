633bbcbbf456996763712a5e1ae6eaa43f24fd36
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2024-06-20 22:21:20 -0600

    ex23, ex24

--- dbg.h
+++ dbg.h
@@ -50,4 +50,5 @@
     goto error;                                                                \
   }
 
+
 #endif
---
+++ duff.h
@@ -0,0 +1,23 @@
+#ifndef __duf_h__
+#define __duf_h__
+
+#define duff_device(...)                                                       \
+  {                                                                            \
+    *to++ = *from++;                                                           \
+  case 7:                                                                      \
+    *to++ = *from++;                                                           \
+  case 6:                                                                      \
+    *to++ = *from++;                                                           \
+  case 5:                                                                      \
+    *to++ = *from++;                                                           \
+  case 4:                                                                      \
+    *to++ = *from++;                                                           \
+  case 3:                                                                      \
+    *to++ = *from++;                                                           \
+  case 2:                                                                      \
+    *to++ = *from++;                                                           \
+  case 1:                                                                      \
+    *to++ = *from++;                                                           \
+  }
+
+#endif
